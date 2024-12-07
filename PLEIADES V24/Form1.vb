f53fedcdda062ce03aa7132a2c0bf7fe308fd50a
john adams <johnadamshome@msn.com>
2024-12-07 17:42:19 +0000

    new26

--- PLEIADES V24/Form1.vb
+++ PLEIADES V24/Form1.vb
@@ -17,10 +17,17 @@ Public Class Form1
     Dim B5
     Dim B6
     Dim B9
+    Dim B8
     Dim B10
-    Dim s
+    Dim B11
+    Dim S As String
     Dim dp
-    Dim currentdp
+    Dim JAA
+    Dim DOMEP
+    Dim H
+
+
+    Private CURRENTDP As Double
 
     Dim SKYD ' DOME POSITION
 
@@ -37,7 +44,7 @@ Public Class Form1
             Try
                 Button1.Text = "CONNECTED"
                 Button1.BackColor = Color.GreenYellow
-                SerialPort1.PortName = "com8"
+                SerialPort1.PortName = "com5"
                 SerialPort1.BaudRate = 9600
                 SerialPort1.DataBits = 8
                 SerialPort1.Open()
@@ -49,6 +56,7 @@ Public Class Form1
                 Button6.Enabled = True
                 Button9.Enabled = True
                 Button8.Enabled = True
+                TextBox3.Text = My.Settings.DOMEP
 
             Catch ex As Exception
                 Button1.Text = "COMMS ERROR"
@@ -90,13 +98,14 @@ Public Class Form1
                 DOME = CreateObject("Thesky64.sky6Dome")
                 MAutoguider = CreateObject("THESKY64.CCDSOFTCAMERA")
                 CAM = CreateObject("THESKY64.CCDSOFTCAMERA")
-                ' Call TELE.CONNECT()
+                Call TELE.CONNECT()
                 Call DOME.connect()
                 B2 = 1
                 BUTTON2.Text = "CONNECTED"
                 BUTTON2.BackColor = Color.GreenYellow
                 Timer2.Enabled = True
                 Button3.Enabled = True
+                Button12.Enabled = True
 
 
             Catch ex As Exception
@@ -105,6 +114,7 @@ Public Class Form1
                 BUTTON2.Text = "CONNECT"
                 Timer2.Enabled = False
                 Button3.Enabled = False
+                Button12.Enabled = False
             End Try
         Else
             B2 = 0
@@ -112,6 +122,7 @@ Public Class Form1
             BUTTON2.BackColor = Color.Transparent
             Timer2.Enabled = False
             Button3.Enabled = False
+            Button12.Enabled = False
         End If
     End Sub
 
@@ -128,13 +139,18 @@ Public Class Form1
         Try
             Call DOME.GETAZEL()
             SKYD = Math.Round(DOME.DAZ, 0)
-            TextBox3.Text = dp
+            TextBox2.Text = SKYD
+            H = DOME.isgotocomplete
+            TextBox4.Text = H
 
         Catch ex As Exception
             MsgBox("DOME COMMS ERROR")
         End Try
+        If B11 = 1 And DateTime.Now > DateTimePicker1.Value Then
 
-
+            Timer6.Enabled = True
+        End If
+        My.Settings.Save()
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
@@ -143,6 +159,7 @@ Public Class Form1
                 Button3.Text = ("CAMERA ON")
                 CAM.CONNECT()
                 MAutoguider.AUTOGUIDER = 1
+                MAutoguider.connect()
                 B3 = 1
                 Button3.BackColor = Color.GreenYellow
             Catch ex As Exception
@@ -155,6 +172,7 @@ Public Class Form1
             B3 = 0
             Button3.Text = "CAMERA OFF"
             CAM.DISCONNECT()
+            MAutoguider.disconnect()
             MAutoguider.AUTOGUIDER = 0
             Button3.BackColor = Color.Transparent
         End If
@@ -163,7 +181,7 @@ Public Class Form1
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.MouseDown
         If B4 = 0 Then
             Button4.Text = "MOVING"
-            SerialPort1.Write("4")
+            SerialPort1.Write("1")
             Button5.Enabled = False
             Button6.Enabled = False
             Button8.Enabled = False
@@ -187,7 +205,7 @@ Public Class Form1
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
         If B5 = 0 Then
             Button5.Text = "MOVING"
-            SerialPort1.Write("3")
+            SerialPort1.Write("2")
             Button4.Enabled = False
             Button6.Enabled = False
             Button8.Enabled = False
@@ -212,7 +230,7 @@ Public Class Form1
         If B6 = 0 Then
             Button6.Text = "DOME OPENING"
             Timer3.Enabled = True
-            SerialPort1.Write("2")
+            SerialPort1.Write("3")
             Button6.Enabled = False
             Button4.Enabled = False
             Button5.Enabled = False
@@ -222,7 +240,7 @@ Public Class Form1
         Else
             Button6.Text = "DOME CLOSING"
             Timer4.Enabled = True
-            SerialPort1.Write("5")
+            SerialPort1.Write("4")
             Button6.Enabled = False
             Button4.Enabled = False
             Button5.Enabled = False
@@ -266,7 +284,7 @@ Public Class Form1
     Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
         If B9 = 0 Then
             Button9.Text = "DUST OPEN"
-            SerialPort1.Write("8")
+            SerialPort1.Write("6")
             Timer5.Enabled = True
             Button9.Enabled = False
             Timer3.Enabled = False
@@ -278,7 +296,7 @@ Public Class Form1
             Button9.BackColor = Color.GreenYellow
         Else
             Button9.Text = "DUST CLOSED"
-            SerialPort1.Write("9")
+            SerialPort1.Write("5")
             Button9.Enabled = False
             Button6.Enabled = False
             Button4.Enabled = False
@@ -301,15 +319,216 @@ Public Class Form1
         Button8.Enabled = True
     End Sub
 
-    Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles Button4.Click
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim S As String = SerialPort1.ReadExisting
+        If S = 0 And B4 = 1 Then
+            My.Settings.CURRENTDP = My.Settings.CURRENTDP - 1
+        End If
+        If S = 0 And B5 = 1 Then
+            My.Settings.CURRENTDP = My.Settings.CURRENTDP + 1
+        End If
+        If S = 9 Then My.Settings.CURRENTDP = My.Settings.HOME
+
+
+        TextBox3.Invoke(New UPDATEDDELEGATE(AddressOf UPDATED), CURRENTDP.ToString)
+
+    End Sub
+    Delegate Sub UPDATEDDELEGATE(ByVal T As String)
+    Public Sub UPDATED(ByVal T As String)
+        DOMEP = Math.Round(My.Settings.CURRENTDP / My.Settings.MAGS * 360)
+
+        If B4 = 1 And DOMEP < 2 Then
+            My.Settings.CURRENTDP = My.Settings.MAGS
+        End If
+        If B5 = 1 And DOMEP > 358 Then
+            My.Settings.CURRENTDP = 0
+
+        End If
+        TextBox3.Text = DOMEP
+        My.Settings.DOMEP = DOMEP
+        My.Settings.Save()
+    End Sub
+
+
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        If B11 = 0 Then
+            B11 = 1
+            Button11.Text = "SHUT ACTIVE"
+            Button11.BackColor = Color.Red
+        Else
+            B11 = 0
+            Button11.Text = "SHUT DOWN"
+            Button11.BackColor = Color.Transparent
+        End If
+    End Sub
+
+    Private Sub Timer6_Tick(sender As Object, e As EventArgs) Handles Timer6.Tick
+        'SHUT DOWN
+        Timer6.Enabled = False
+        Timer1.Enabled = False
+        Timer2.Enabled = False
+        ' STOP TRACKING
+        B8 = 0
+        Button8.Text = "TRACK"
+        Button8.BackColor = Color.Transparent
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button9.Enabled = True
+        SerialPort1.Write("8")
+        Timer7.Enabled = False
+        B4 = 0
+        B5 = 0
+        ' CAMERA OFF
+        B3 = 0
+        Button3.Text = "CAMERA OFF"
+        CAM.DISCONNECT()
+        MAutoguider.disconnect()
+        Button3.BackColor = Color.Transparent
+        ' DOME CLOSE
+        Button6.Text = "DOME CLOSING"
+        Timer4.Enabled = True
+        SerialPort1.Write("4")
+        Button6.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+        B6 = 0
+        'park scope
+        Timer8.Enabled = True
+
 
     End Sub
 
     Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If B8 = 0 Then
+            B8 = 1
+            Button8.Text = "TRACKING"
+            Button8.BackColor = Color.GreenYellow
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button9.Enabled = False
+            Timer7.Enabled = True
+        Else
+            B8 = 0
+            Button8.Text = "TRACK"
+            Button8.BackColor = Color.Transparent
+            Button4.Enabled = True
+            Button5.Enabled = True
+            Button6.Enabled = True
+            Button9.Enabled = True
+            SerialPort1.Write("8")
+            Timer7.Enabled = False
+            B4 = 0
+            B5 = 0
+        End If
+    End Sub
+
+    Private Sub Timer7_Tick(sender As Object, e As EventArgs) Handles Timer7.Tick
+        'TRACKING
+        DOMEP = Math.Round(My.Settings.CURRENTDP / My.Settings.MAGS * 360)
+        H = DOME.isgotocomplete
+
+        Do Until H = 1
+            TextBox4.Text = H
+            '
+            H = DOME.isgotocomplete
+        Loop
+
+        Dim SYNA
+        Dim SYNB
+        Dim SYNC
+        Dim SYNE
+        Dim SYN
+
+        SYN = SKYD
+        SYNA = Math.Abs(DOMEP - SYN)
+
+        If SYNA < 3 Then
+            ' Button4.Text = "WEST"
+
+            ' Button4.BackColor = Color.Transparent
+            ' Button5.BackColor = Color.Transparent
+            B4 = 0
+            ' Button5.Enabled = True
+            SerialPort1.Write("8")
+            ' Button5.Text = "EAST"
+            B5 = 0
+            ' Button6.Enabled = True
+            ' SerialPort1.Write("8")
+        Else
+
+
+
+
+            SYNB = (SYN + 360) Mod 360
+            SYNC = (DOMEP + 360) Mod 360
+
+            SYNE = SYNC - SYNB
+
+
+
+
+            If SYNE > 0 Then
+
+                ' Button4.Text = "MOVING"
+                SerialPort1.Write("1")
+                '  Button5.Enabled = False
+                B4 = 1
+                ' Button7.BackColor = Color.GreenYellow
+            Else
+
+
+                ' Button5.Text = "MOVING"
+                SerialPort1.Write("2")
+                ' Button6.Enabled = False
+                B5 = 1
+                'Button7.BackColor = Color.Transparent
+
+
+
+
+            End If
+        End If
+    End Sub
+
+    Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles Button4.Click
 
     End Sub
 
-    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
-        dp = dp + 1
+    Private Sub Timer8_Tick(sender As Object, e As EventArgs) Handles Timer8.Tick
+        'park scope
+        Call TELE.PARK
+        Timer8.Enabled = False
+        Timer9.Enabled = True
+        B11 = 0
+        Button11.Text = "COMPLETED"
+        Button11.BackColor = Color.GreenYellow
+    End Sub
+
+    Private Sub Timer9_Tick(sender As Object, e As EventArgs) Handles Timer9.Tick
+        'close cover
+        Button9.Text = "DUST CLOSED"
+        SerialPort1.Write("5")
+        Button9.Enabled = False
+        Button6.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button8.Enabled = False
+        Timer5.Enabled = True
+        Button9.BackColor = Color.Transparent
+        B9 = 0
+        Timer9.Enabled = False
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Call TELE.FINDHOME()
     End Sub
 End Class
--- PLEIADES V24/Form2.vb
+++ PLEIADES V24/Form2.vb
@@ -3,16 +3,22 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         My.Settings.MAGS = NumericUpDown1.Value
         My.Settings.HOME = NumericUpDown2.Value
+        My.Settings.compo = TextBox1.Text
         My.Settings.Save()
         Me.Hide()
 
 
-
+        TextBox1.Text = My.Settings.compo
 
     End Sub
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         NumericUpDown1.Value = My.Settings.MAGS
         NumericUpDown2.Value = My.Settings.HOME
+
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+
     End Sub
 End Class
